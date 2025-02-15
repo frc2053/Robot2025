eddea30b31954e78d1b01187d4a5683d4c57ebd3
Drew Williams <williams.r.drew@gmail.com>
2025-02-15 14:55:52 -0500

    fix rev pdh can id

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -37,7 +37,7 @@ class Robot : public frc::TimedRobot {
   frc2::Command* m_autonomousCommand = nullptr;
 
   RobotContainer m_container;
-  frc::PowerDistribution pdp{0, frc::PowerDistribution::ModuleType::kRev};
+  frc::PowerDistribution pdp{1, frc::PowerDistribution::ModuleType::kRev};
 
   units::second_t lastTotalLoopTime;
   nt::DoublePublisher loopTimePub{nt::NetworkTableInstance::GetDefault()
