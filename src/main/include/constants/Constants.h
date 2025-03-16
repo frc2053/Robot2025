3c92f660e64c2303fa86935eb5f1ef1d9a8407b8
Drew Williams <williams.r.drew@gmail.com>
2025-03-16 07:58:46 -0400

    made reef only json

--- src/main/include/constants/Constants.h
+++ src/main/include/constants/Constants.h
@@ -6,6 +6,7 @@
 
 #include <frc/apriltag/AprilTagFieldLayout.h>
 
+#include "frc/Filesystem.h"
 #include "frc/apriltag/AprilTag.h"
 #include "frc/apriltag/AprilTagFields.h"
 #include "frc/geometry/Pose3d.h"
@@ -15,9 +16,9 @@
 #include "frc/geometry/Translation3d.h"
 
 namespace consts::yearspecific {
-inline const frc::AprilTagFieldLayout TAG_LAYOUT =
-    frc::AprilTagFieldLayout::LoadField(
-        frc::AprilTagField::k2025ReefscapeWelded);
+// inline const frc::AprilTagFieldLayout TAG_LAYOUT =
+//     frc::AprilTagFieldLayout::LoadField(
+//         frc::AprilTagField::k2025ReefscapeWelded);
 
 inline constexpr units::inch_t CLAW_OFFSET_L = 0_in;
 inline constexpr units::inch_t CLAW_OFFSET_R = 0_in;
@@ -25,21 +26,6 @@ inline constexpr frc::Transform2d CLAW_TRANS_L{0_m, CLAW_OFFSET_L,
                                                frc::Rotation2d{}};
 inline constexpr frc::Transform2d CLAW_TRANS_R{0_m, CLAW_OFFSET_R,
                                                frc::Rotation2d{}};
-// inline const frc::AprilTag first{
-//     19,
-//     frc::Pose3d{frc::Translation3d{4.073905999999999_m, 4.745482_m,
-//     0.308102_m},
-//                 frc::Rotation3d{frc::Quaternion{0.5000000000000001, 0.0, 0.0,
-//                                                 0.8660254037844386}}}};
-// inline const frc::AprilTag second{
-//     20,
-//     frc::Pose3d{frc::Translation3d{4.904739999999999_m, 4.745482_m,
-//     0.308102_m},
-//                 frc::Rotation3d{frc::Quaternion{0.8660254037844387, 0.0, 0.0,
-//                                                 0.49999999999999994}}}};
-// inline const frc::AprilTag third{
-//     21, frc::Pose3d{frc::Translation3d{5.321046_m, 4.0259_m, 0.308102_m},
-//                     frc::Rotation3d{frc::Quaternion{1.0, 0.0, 0.0, 0.0}}}};
-// inline const frc::AprilTagFieldLayout TAG_LAYOUT =
-//     frc::AprilTagFieldLayout({first, second, third}, 17.548_m, 8.052_m);
+inline const frc::AprilTagFieldLayout TAG_LAYOUT = frc::AprilTagFieldLayout(
+    frc::filesystem::GetDeployDirectory() + "/reefonlytags.json");
 }  // namespace consts::yearspecific
