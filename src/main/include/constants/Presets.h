6550ab090c04e0ec15ea42e12b0e522c45e35eb5
Drew Williams <williams.r.drew@gmail.com>
2025-03-26 10:14:05 -0400

    before we leave

--- src/main/include/constants/Presets.h
+++ src/main/include/constants/Presets.h
@@ -12,9 +12,9 @@ namespace wrist {
 namespace coral {
 inline constexpr units::radian_t loading = 87_deg;
 inline constexpr units::radian_t l1 = -56_deg;
-inline constexpr units::radian_t l2 = -86.5_deg;
-inline constexpr units::radian_t l3 = -86.5_deg;
-inline constexpr units::radian_t l4 = -88_deg;
+inline constexpr units::radian_t l2 = -83_deg;
+inline constexpr units::radian_t l3 = -83_deg;
+inline constexpr units::radian_t l4 = -83_deg;
 }  // namespace coral
 
 inline constexpr units::radian_t algaeHold = 10_deg;
